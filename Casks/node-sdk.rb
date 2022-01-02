c5008aca24709b29272fe319bb4d0c6781e6e050
Andrew Carter <asc@alpental.io>
2022-01-02 11:55:51 -0800

    Update Node.js 17.3

--- Casks/node-sdk.rb
+++ Casks/node-sdk.rb
@@ -1,6 +1,6 @@
 cask 'node-installer' do
-  version '14.15.0'
-  sha256 'e2eb6497d090926a6c4cce972aefe9a91beb53c495d6e38d91de850ae0c22b61'
+  version '17.3.0'
+  sha256 'f960b616eace93d171a0d15e67c62116aa3c6100eb6266046774cc478487b9d9'
 
   url "https://nodejs.org/dist/v#{version}/node-v#{version}.pkg"
   appcast 'https://github.com/nodejs/node/releases.atom'
@@ -8,7 +8,7 @@ cask 'node-installer' do
   homepage 'https://nodejs.org'
 
   pkg "node-v#{version}.pkg"
-  
+
   uninstall pkgutil: [
     'org.nodejs.npm.pkg',
     'org.nodejs.node.pkg'
