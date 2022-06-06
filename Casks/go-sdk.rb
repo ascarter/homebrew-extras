31cfad2625d1d1386a1bd9ed3f35fbc6ade224ba
Andrew Carter <ascarter@users.noreply.github.com>
2022-06-06 14:38:45 -0700

    Update Go 1.18.3

--- Casks/go-sdk.rb
+++ Casks/go-sdk.rb
@@ -1,10 +1,10 @@
 cask 'go-sdk' do
-  version '1.18'
+  version '1.18.3'
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
   if Hardware::CPU.intel?
-    sha256 'dbfabbbb6377c225ccf28393ecc7aed55564870fcfb39d797411d6b8693013c6'
+    sha256 'a23a24c5528671d444328a36a98056902f699a5a211b6ad5db29ca0c012e0085'
   else
-    sha256 '7c48130b37a155e709ef1db22d3f40ca17cfdaee0dc9d0cc35d2d7c4e0e3d299'
+    sha256 '0b1683bdb449b82d8476a8d13cdc0f41a5c1bc3d1c85d37852909e5b3f3bbd81'
   end
 
   url "https://go.dev/dl/go#{version}.darwin-#{arch}.pkg"
