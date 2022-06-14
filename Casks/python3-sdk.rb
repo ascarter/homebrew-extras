41af10274be9c758e98f3ac6742eecac74d02bb7
Andrew Carter <ascarter@users.noreply.github.com>
2022-06-13 21:22:07 -0700

    Update python3.10.5

--- Casks/python3-sdk.rb
+++ Casks/python3-sdk.rb
@@ -1,6 +1,6 @@
 cask 'python3-sdk' do
-  version '3.10.1'
-  sha256 'f3837587ed22d23689d390eaf0892741362adcea25771beed8e753cc71e1e3ec'
+  version '3.10.5'
+  sha256 '36ff0f2ef1fdd8603042576d206224a76db32a0000e8332877751f86d98abc53'
 
   url "https://www.python.org/ftp/python/#{version}/python-#{version}-macos11.pkg"
   name 'Python Programming Language'
