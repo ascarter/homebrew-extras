a6ed9441ae95aa8fe23b33b8eef3b9ff320165b5
Andrew Carter <ascarter@users.noreply.github.com>
2025-09-06 09:54:27 -0700

    Add cask for unsigned Fedora Media Writer

---
+++ Casks/fedora-media-writer-nosign.rb
@@ -0,0 +1,31 @@
+cask "fedora-media-writer-nosign" do
+  arch arm: "-arm64"
+
+  version "5.2.8"
+  sha256 arm:   "06e4762a52358c5e3f382539b1e29cc838381b3a4ad7ec8995f2cbba7cfbf396",
+         intel: "5bcfa5c302133aa97b941d1a7b569f321febc3af0693bbc2b8ee55c582f8a5e2"
+
+  url "https://github.com/FedoraQt/MediaWriter/releases/download/#{version}/FedoraMediaWriter-osx#{arch}-#{version}.dmg",
+      verified: "github.com/FedoraQt/MediaWriter/"
+  name "Fedora Media Writer"
+  desc "Tool to write Fedora images to portable media files. This version is unsigned with Gatekeeper checks disabled."
+  homepage "https://docs.fedoraproject.org/en-US/quick-docs/creating-and-using-a-live-installation-image/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :mojave"
+
+  app "FedoraMediaWriter.app"
+
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-cr", "#{appdir}/FedoraMediaWriter.app"]
+  end
+
+  zap trash: [
+    "~/Library/Caches/fedoraproject.org",
+    "~/Library/Saved Application State/org.fedoraproject.MediaWriter.savedState",
+  ]
+end
